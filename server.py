b02e3be3174b53f7fee3a997db65c1e882ffecc7
hirasaki1985 <hirasaki1985@gmail.com>
2019-08-23 14:57:53 +0900

    評価APIの枠組み作成

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, jsonify
+from app import controller
+
 app = Flask(__name__)
 
 @app.route('/')
 def web_page():
   return render_template('index.html')
+
+@app.route('/api/evaluate', methods=['GET', 'POST'])
+def evaluate():
+  controller.evaluate
+  return jsonify({"test":"test"})
 
 if __name__ == '__main__':
   app.run()
